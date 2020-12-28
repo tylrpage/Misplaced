106d46c8d8fb1de0064e3c38b57e57ff314024ea
dj46563 <tylrpage@gmail.com>
2020-12-28 15:44:46 -0800

    Created test client script. Moved scripts to scripts folder

---
+++ Out of Place URP/Assets/Scripts/Client.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror.SimpleWeb;
+
+public class Client : MonoBehaviour
+{
+    private SimpleWebClient _webClient;
+
+    private void Awake()
+    {
+        TcpConfig tcpConfig = new TcpConfig(true, 5000, 20000);
+        _webClient = SimpleWebClient.Create(16*1024, 1000, tcpConfig);
+        _webClient.onConnect += delegate
+        {
+            Debug.Log("Client connected");
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes("Hello from client");
+            _webClient.Send(new ArraySegment<byte>(bytes));
+        };
+        _webClient.onData += delegate(ArraySegment<byte> bytes)
+        {
+            string msg = System.Text.Encoding.UTF8.GetString(bytes.Array);
+            Debug.Log("Received message : " + msg);
+        };
+        
+        // connect
+        UriBuilder uriBuilder = new UriBuilder()
+        {
+            Scheme = "wss",
+            Host = "tylrpage.com",
+            Port = Constants.GAME_PORT
+        };
+        _webClient.Connect(uriBuilder.Uri);
+    }
+
+    private void LateUpdate()
+    {
+        _webClient.ProcessMessageQueue(this);
+    }
+}
--- Out of Place URP/Assets/Scripts/Utils/Constants.cs
+++ Out of Place URP/Assets/Scripts/Utils/Constants.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public static class Constants
 {
+    // SERVER + CLIENT
+    public static readonly ushort GAME_PORT = 9001;
+    
+    // CLIENT
     public static readonly int GRID_WIDTH = 10;
     public static readonly int GRID_HEIGHT = 10;
     public static readonly int GRID_UNITS = 1;
